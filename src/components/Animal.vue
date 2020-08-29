c84000f9d958b69b4cc39db2e5c1cea0d3bfc30f
Ehud Perlman <ehudperlman@MacBook-Pro.local>
2020-08-29 19:19:55 +0300

    Added Animal cmp and improved AnimalList cmp

---
+++ src/components/Animal.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="card">
+        <img :src="props.animal.imageUrl" alt="Avatar" />
+        <div class="container">
+            <h4>
+                <b>{{ props.animal.name }}</b>
+            </h4>
+            <p>{{ props.animal.type }}</p>
+            <p>{{ props.animal.dateOfBirth }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Animal',
+    props: {
+        animal: Object,
+    },
+    setup(props) {
+        return {
+            props,
+        };
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    transition: 0.3s;
+    border-radius: 5px; /* 5px rounded corners */
+    max-width: 200px;
+    margin: 20px;
+}
+
+img {
+    width: 100%;
+    border-radius: 5px 5px 0 0;
+}
+</style>
--- src/components/AnimalsList.vue
+++ src/components/AnimalsList.vue
@@ -3,7 +3,7 @@
         <p>Animals List!</p>
         <ul>
             <li v-for="(animal, index) in animals" :key="index">
-                {{ animal }}
+                <Animal :animal="animal" />
             </li>
         </ul>
     </div>
@@ -11,21 +11,69 @@
 
 <script>
 import { ref } from 'vue';
+import Animal from './Animal.vue';
 
 export default {
     name: 'AnimalsList',
-
+    components: {
+        Animal,
+    },
     setup() {
         const animals = ref([
-            'Kidney',
-            'Kfitz',
-            'Peter',
-            'Pizi',
-            'Alona',
-            'Andy',
-            'Libi',
-            'Pumi',
-            'Haim',
+            {
+                name: 'Kidney',
+                type: 'Cat',
+                dateOfBirth: new Date('2005-05-05').toLocaleDateString(),
+                imageUrl: require('../assets/cat.jpeg'),
+            },
+            {
+                name: 'Kfitz',
+                type: 'Cat',
+                dateOfBirth: new Date('2007-05-05').toLocaleDateString(),
+                imageUrl: require('../assets/cat.jpeg'),
+            },
+            {
+                name: 'Peter',
+                type: 'Cat',
+                dateOfBirth: new Date('2010-05-05').toLocaleDateString(),
+                imageUrl: require('../assets/cat.jpeg'),
+            },
+            {
+                name: 'Pizi',
+                type: 'Cat',
+                dateOfBirth: new Date('2012-05-05').toLocaleDateString(),
+                imageUrl: require('../assets/cat.jpeg'),
+            },
+            {
+                name: 'Alona',
+                type: 'Cat',
+                dateOfBirth: new Date('2014-05-05').toLocaleDateString(),
+                imageUrl: require('../assets/cat.jpeg'),
+            },
+            {
+                name: 'Andey',
+                type: 'Cat',
+                dateOfBirth: new Date('2016-05-05').toLocaleDateString(),
+                imageUrl: require('../assets/cat.jpeg'),
+            },
+            {
+                name: 'Libi',
+                type: 'Cat',
+                dateOfBirth: new Date('2018-05-05').toLocaleDateString(),
+                imageUrl: require('../assets/cat.jpeg'),
+            },
+            {
+                name: 'Pumi',
+                type: 'Cat',
+                dateOfBirth: new Date('2019-05-05').toLocaleDateString(),
+                imageUrl: require('../assets/cat.jpeg'),
+            },
+            {
+                name: 'Haim',
+                type: 'Cat',
+                dateOfBirth: new Date('2020-05-05').toLocaleDateString(),
+                imageUrl: require('../assets/cat.jpeg'),
+            },
         ]);
         return {
             animals,
@@ -34,4 +82,13 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+ul {
+    display: flex;
+    flex-wrap: wrap;
+
+    li {
+        margin: 0 auto;
+    }
+}
+</style>
