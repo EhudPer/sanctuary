7e747bcf7f92f65b27ce644f5353bcfc34b92722
Ehud Perlman <ehudperlman@MacBook-Pro.local>
2020-08-30 10:49:39 +0300

    Added back-end server and related files

---
+++ server/src/index.ts
@@ -0,0 +1,35 @@
+import express = require('express');
+import { ApolloServer, gql } from 'apollo-server-express';
+
+const app = express();
+
+const schema = gql`
+    type Query {
+        me: User
+    }
+
+    type User {
+        username: String!
+    }
+`;
+
+const resolvers = {
+    Query: {
+        me: () => {
+            return {
+                username: 'Ehud Perlman',
+            };
+        },
+    },
+};
+
+const server = new ApolloServer({
+    typeDefs: schema,
+    resolvers,
+});
+
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen({ port: 8000 }, () => {
+    console.log('Apollo Server on http://localhost:8000/graphql');
+});
