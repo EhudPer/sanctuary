40942836f5d4ba82b4ec153b845f3738466c68ff
Ehud Perlman <ehudperlman@MacBook-Pro.local>
2020-08-30 18:31:36 +0300

    Added Hard coded data to server and improved schema and resolvers

--- front-end/src/components/AnimalsList.vue
+++ front-end/src/components/AnimalsList.vue
@@ -24,55 +24,64 @@ export default {
                 name: 'Kidney',
                 type: 'Cat',
                 dateOfBirth: new Date('2005-05-05').toLocaleDateString(),
-                imageUrl: require('../assets/cat.jpeg'),
+                imageUrl:
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSlNyI5Bbsl1vq1BQjH9XA-Z4j0Kkk0cEpAnA&usqp=CAU',
             },
             {
                 name: 'Kfitz',
                 type: 'Cat',
                 dateOfBirth: new Date('2007-05-05').toLocaleDateString(),
-                imageUrl: require('../assets/cat.jpeg'),
+                imageUrl:
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSlNyI5Bbsl1vq1BQjH9XA-Z4j0Kkk0cEpAnA&usqp=CAU',
             },
             {
                 name: 'Peter',
                 type: 'Cat',
                 dateOfBirth: new Date('2010-05-05').toLocaleDateString(),
-                imageUrl: require('../assets/cat.jpeg'),
+                imageUrl:
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSlNyI5Bbsl1vq1BQjH9XA-Z4j0Kkk0cEpAnA&usqp=CAU',
             },
             {
                 name: 'Pizi',
                 type: 'Cat',
                 dateOfBirth: new Date('2012-05-05').toLocaleDateString(),
-                imageUrl: require('../assets/cat.jpeg'),
+                imageUrl:
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSlNyI5Bbsl1vq1BQjH9XA-Z4j0Kkk0cEpAnA&usqp=CAU',
             },
             {
                 name: 'Alona',
                 type: 'Cat',
                 dateOfBirth: new Date('2014-05-05').toLocaleDateString(),
-                imageUrl: require('../assets/cat.jpeg'),
+                imageUrl:
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSlNyI5Bbsl1vq1BQjH9XA-Z4j0Kkk0cEpAnA&usqp=CAU',
             },
             {
                 name: 'Andey',
                 type: 'Cat',
                 dateOfBirth: new Date('2016-05-05').toLocaleDateString(),
-                imageUrl: require('../assets/cat.jpeg'),
+                imageUrl:
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSlNyI5Bbsl1vq1BQjH9XA-Z4j0Kkk0cEpAnA&usqp=CAU',
             },
             {
                 name: 'Libi',
                 type: 'Cat',
                 dateOfBirth: new Date('2018-05-05').toLocaleDateString(),
-                imageUrl: require('../assets/cat.jpeg'),
+                imageUrl:
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSlNyI5Bbsl1vq1BQjH9XA-Z4j0Kkk0cEpAnA&usqp=CAU',
             },
             {
                 name: 'Pumi',
                 type: 'Cat',
                 dateOfBirth: new Date('2019-05-05').toLocaleDateString(),
-                imageUrl: require('../assets/cat.jpeg'),
+                imageUrl:
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSlNyI5Bbsl1vq1BQjH9XA-Z4j0Kkk0cEpAnA&usqp=CAU',
             },
             {
                 name: 'Haim',
                 type: 'Cat',
                 dateOfBirth: new Date('2020-05-05').toLocaleDateString(),
-                imageUrl: require('../assets/cat.jpeg'),
+                imageUrl:
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSlNyI5Bbsl1vq1BQjH9XA-Z4j0Kkk0cEpAnA&usqp=CAU',
             },
         ]);
         return {
