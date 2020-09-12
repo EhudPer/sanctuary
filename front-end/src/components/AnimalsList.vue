de4de2873d56576a7d684fad24094accc4830319
Ehud Perlman <ehudperlman@MacBook-Pro.local>
2020-09-12 10:05:17 +0300

    Connected frontend and backend

--- front-end/src/components/AnimalsList.vue
+++ front-end/src/components/AnimalsList.vue
@@ -1,41 +1,44 @@
 <template>
-    <div class="animals-list">
-        <p>Animals List!</p>
-        <ul>
-            <li v-for="animal in animals" :key="animal._id">
-                <Animal :animal="animal" />
-            </li>
-        </ul>
-    </div>
+  <div class="animals-list">
+    <p>Animals List!</p>
+    <ul>
+      <li v-for="animal in animals" :key="animal._id">
+        <Animal :animal="animal" />
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
-import { useStore } from 'vuex';
-import Animal from './Animal.vue';
+import { defineComponent, computed } from "vue";
+import { useStore } from "vuex";
+import Animal from "./Animal.vue";
 
 export default defineComponent({
-    name: 'AnimalsList',
-    components: {
-        Animal,
-    },
-    setup() {
-      const store = useStore();
-        const animals = ref(store.state.animals)
-        return {
-            animals,
-        };
-    },
+  name: "AnimalsList",
+  components: {
+    Animal,
+  },
+  setup() {
+    const store = useStore();
+    store.dispatch("fetchAnimals");
+    //add loader for wait time until dispatch is done.
+    //add getter instead of directly accessing state.
+    const animals = computed(() => store.state.animals);
+    return {
+      animals,
+    };
+  },
 });
 </script>
 
 <style lang="scss" scoped>
 ul {
-    display: flex;
-    flex-wrap: wrap;
+  display: flex;
+  flex-wrap: wrap;
 
-    li {
-        margin: 0 auto;
-    }
+  li {
+    margin: 0 auto;
+  }
 }
 </style>
