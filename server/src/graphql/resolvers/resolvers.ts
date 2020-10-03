0176303b43b9f1aef54a744ca5bfd644c8a0f707
Ehud Perlman <ehudperlman83@gmail.com>
2020-10-03 15:45:27 +0300

    Changed to async await and more error handling

--- server/src/graphql/resolvers/resolvers.ts
+++ server/src/graphql/resolvers/resolvers.ts
@@ -1,23 +1,23 @@
-import { AnimalModel } from "../../models/animal";
+import { AnimalModel } from '../../models/animal';
 
 // Remember to implement error handaling in back and front end
 const getAnimals = async () => AnimalModel.find({});
 
 const getAnimal = async (root, data: any) =>
-  AnimalModel.findOne({ _id: data._id });
+    AnimalModel.findOne({ _id: data._id });
 
 const updateAnimal = async (root, { _id, input }) =>
-  AnimalModel.findOneAndUpdate({ _id }, input);
+    AnimalModel.findOneAndUpdate({ _id }, input);
 
 // Remember later to split resolvers to different files.
 const resolvers = {
-  Query: {
-    animals: getAnimals,
-    animal: getAnimal,
-  },
-  Mutation: {
-    updateAnimal,
-  },
+    Query: {
+        animals: getAnimals,
+        animal: getAnimal
+    },
+    Mutation: {
+        updateAnimal
+    }
 };
 
 export default resolvers;
