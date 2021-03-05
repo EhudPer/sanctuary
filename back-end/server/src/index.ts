399232b8c8e3a8091c6689286406a5c1c5212f1a
Ehud Perlman <ehudperlman@MacBook-Pro.local>
2021-03-05 16:43:46 +0200

    Minor console logs deletion

--- back-end/server/src/index.ts
+++ back-end/server/src/index.ts
@@ -1,5 +1,4 @@
 import express = require("express");
-// import * as mongoose from "mongoose";
 import mongoose = require("mongoose");
 import { ApolloServer } from "apollo-server-express";
 import * as dotenv from "dotenv";
@@ -7,7 +6,6 @@ import * as dotenv from "dotenv";
 import { isAuth } from "./middleware/is-auth";
 import schema from "./graphql/schema/schema";
 import resolvers from "./graphql/resolvers/resolvers";
-// import path from "path";
 import path = require("path");
 
 if (process.env.NODE_ENV !== "production") {
@@ -22,14 +20,8 @@ const start = async () => {
   app.use(isAuth);
 
   if (process.env.NODE_ENV === "production") {
-    // app.use(express.static(path.join(__dirname, "./dist")));
     app.use(express.static("dist"));
-    // app.get("/*", (req, res) => {
-    // app.get("/", (req, res) => {
-    // res.sendFile(path.join(__dirname, "./dist", "index.html"));
     app.get("*", (req, res) => {
-      // res.sendFile(path.join(__dirname, "./index.html"));
-      // res.sendFile(path.resolve(__dirname, "dist", "index.html"));
       res.sendFile(path.resolve(__dirname + "/../dist/index.html"));
     });
   }
--- front-end/src/store/index.ts
+++ front-end/src/store/index.ts
@@ -177,7 +177,6 @@ const actions = {
     { commit }: { commit: any },
     { token }: { token: string }
   ) {
-    console.log("store id token", token);
     try {
       const response = await graphqlClient.query({
         query: gql`
