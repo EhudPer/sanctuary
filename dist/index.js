8a5472c9be1f21bb555f1cb9687ab7b6a56cc36b
Ehud Perlman <ehudperlman@MacBook-Pro.local>
2021-03-05 15:28:41 +0200

    Fixing for heroku

--- dist/index.js
+++ dist/index.js
@@ -21,8 +21,7 @@ const start = () => tslib_1.__awaiter(void 0, void 0, void 0, function* () {
     if (process.env.NODE_ENV === "production") {
         app.use(express.static(path.join(__dirname, "./dist")));
         app.get("/*", (req, res) => {
-            // res.sendFile(path.join(__dirname, "./dist", "index.html"));
-            res.sendFile(path.join(__dirname, "../front-end/dist", "index.html"));
+            res.sendFile(path.join(__dirname, "./dist", "index.html"));
         });
     }
     mongoose.connect(MONGO_URL, {
