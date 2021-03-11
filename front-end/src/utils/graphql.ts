eb9ac8a042774b2385d2a8bcd194694402437624
Ehud Perlman <ehudperlman@MacBook-Pro.local>
2021-03-11 14:46:06 +0200

    Changed apolloserver api call for ssl

--- front-end/src/utils/graphql.ts
+++ front-end/src/utils/graphql.ts
@@ -6,7 +6,7 @@ export default new ApolloClient({
   // Provide the URL to the API server.
   link: new HttpLink({
     // uri: "http://localhost:8000/graphql",
-    uri: "https://localhost:8000/graphql",
+    uri: "http://localhost:8000/graphql",
   }),
   // Using a cache for blazingly
   // fast subsequent queries.
