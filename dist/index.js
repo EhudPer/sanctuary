957b1ce5bd21f44211ab67791ab51476c92ba002
Ehud Perlman <ehudperlman@MacBook-Pro.local>
2021-03-21 21:01:26 +0200

    Ready heroku

--- dist/index.js
+++ dist/index.js
@@ -202,7 +202,7 @@ const start = () => tslib_1.__awaiter(void 0, void 0, void 0, function* () {
     }
     server.listen({ port: process.env.PORT || 8000 }, () => {
         // server.listen({ port: config.port }, () => {
-        console.log("🚀 Server ready at", `http${process.env.NODE_ENV === "production@" ? "s" : ""}://${process.env.NODE_ENV === "production@"
+        console.log("🚀 Server ready at", `http${process.env.NODE_ENV === "production" ? "s" : ""}://${process.env.NODE_ENV === "production"
             ? "sanctuary-app.herokuapp.com"
             : "localhost"}:${process.env.PORT || 8000}${apollo.graphqlPath}`);
     });
