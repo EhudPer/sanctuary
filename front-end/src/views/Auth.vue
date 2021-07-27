38786a04fc563c4790bbd6070751849c6e7acced
Ehud Perlman <ehudperlman@MacBook-Pro.local>
2021-07-27 17:40:58 +0300

    Minor google login code change in auth component

--- front-end/src/views/Auth.vue
+++ front-end/src/views/Auth.vue
@@ -156,7 +156,9 @@ export default defineComponent({
 
       const token = googleUser.uc
         ? googleUser.uc.id_token
-        : googleUser.tc.id_token;
+        : googleUser.tc
+        ? googleUser.tc.id_token
+        : googleUser.Zb.id_token;
 
       //try moving the flow all the way to db just with using the ggogle token and the clip
       // so i can crEATE the user or get it from the db and return a token anyhow to local storage a
